9e6c17cf7c8e1ec30d0c5699620734a3ec712ef2
Annie1790 <budayaniko@outlook.com>
2022-11-27 13:18:11 +0000

    js done

--- index.js
+++ index.js
@@ -1,37 +1,41 @@
+const canvas = document.querySelector("#sketchCanvas");
+const currentSize = 32;
+
 function createCube() {
-    const canvas = document.querySelector("#sketchCanvas");
-    const cubes = document.createElement("div")
-    cubes.classList.add("hoverColor")
+    const cubes = document.createElement("div");
+    cubes.classList.add("hoverColor");
     cubes.style.height = 16;
     cubes.style.width = 16;
     cubes.style.border = "solid 0.5px gray"
     canvas.appendChild(cubes)
 }
 function createGrid(number) {
-    const sketch = document.querySelector("#sketchCanvas");
-    sketch.style.gridTemplateColumns = `repeat(${number}, 1fr)`
-    sketch.style.gridTemplateRows = `repeat(${number}, 1fr)`
+    canvas.innerHTML = "";
+    canvas.style.gridTemplateColumns = `repeat(${number}, 1fr)`
+    canvas.style.gridTemplateRows = `repeat(${number}, 1fr)`
     let totalCubes = number * number
     for (let i = 0; i < totalCubes; i++) {
         createCube()
     }
-}
-createGrid(32)
-
-const color = document.querySelectorAll(".hoverColor")
-for (let i = 0; i < color.length; i++) {
-    color[i].addEventListener("mouseover", function () {
-        color[i].style.backgroundColor = "black";
-    })
+    hover();
 }
 
+function hover() {
+    const color = document.querySelectorAll(".hoverColor")
+    for (let i = 0; i < color.length; i++) {
+        color[i].addEventListener("mouseover", function () {
+            color[i].style.backgroundColor = "black";
+        })
+    }}
+
+createGrid(currentSize);
 
 const button = document.querySelector("#button");
 button.addEventListener("click", function () {
-    let number = prompt("Enter a number", 0);
-    Number(number);
-    if (number < 51) {
-        createGrid(number);
+    let newNumOfGrid = prompt("Enter a number", 0);
+    Number(newNumOfGrid);
+    if (newNumOfGrid < 51) {
+        createGrid(newNumOfGrid);
     } else {
         alert("Canvas is too big!")
     }
